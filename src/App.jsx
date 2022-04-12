89c60c9cf922a1bed8b39d6204a62f79d85b6dd1
mckinseyleckenby <leckenbym11@gmail.com>
2022-04-12 18:57:55 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import About from "./components/about/About"
 import Contact from "./components/contact/Contact";
 import Toggle from "./components/toggle/Toggle"
 
+
 const App = () => {
   const [dark, setDark] = useState(false)
 return <div>
+
   <Toggle/>
   <Intro/>
   <About/>
   <Contact/>
+
   </div>;
 };
 
