17f9cde16b8a4d29f012cd0e7d160be4c808c9fe
mckinseyleckenby <leckenbym11@gmail.com>
2022-04-12 23:00:32 -0500

    media query done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useContext } from 'react'
 import "./contact.css"
 import Phone from "../../img/phone.png"
 import Email from "../../img/email.png"
 import Location from "../../img/location.png"
 import emailjs from '@emailjs/browser';
+import { ThemeContext } from "../../context";
+
 
 export const Contact = () => {
     const formRef = useRef()
     const [done, setDone] = useState(false)
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -30,7 +34,7 @@ return (
         <div className='c-bg'></div>
         <div className='c-wrapper'>
             <div className='c-left'>
-                <h1 className='c-title'>Lets Discuss your project</h1>
+                <h1 className='c-title'>Lets Discuss Your Project</h1>
                 <div className='c-info'>
                     <div className='c-info-item'>
                         <img src={Phone} alt="phone" className='c-icon' />
@@ -52,10 +56,10 @@ return (
                     freelancing if the right project comes along.
                 </p>
                 <form ref={formRef} onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Name" name="user_name" />
-                    <input type="text" placeholder="Subject" name="user_subject" />
-                    <input type="text" placeholder="Email" name="user_email" />
-                    <textarea rows="5" placeholder="Message" name="message" />
+                    <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
+                    <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
+                    <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
+                    <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
                     <button>Submit</button>
                     {done && "Thank You..."}
                 </form>
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,7 +7,7 @@ export const Intro = () => {
         <div className='i'>
             <div className='i-left'>
                 <div className='i-left-wrapper'>
-                    <h2 className='i-intro'>Hello, My name is</h2>
+                    <h2 className='i-intro'>Hello, my name is</h2>
                     <h1 className='i-name'>McKinsey Leckenby</h1>
                     <div className='i-title'>
                         <div className='i-title-wrapper'>
